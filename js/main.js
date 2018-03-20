d021ee5e7d707fb023cfbf2fe26c2ee8affc320d
LoggeL <hyper.xjo@gmx.de>
2018-03-20 01:18:03 +0100

    Added sticky search, Scripts

--- js/main.js
+++ js/main.js
@@ -1,22 +1,46 @@
 var champData = []
+var scriptData = {}
+var version
 
 function setTheme(overwrite = false) {
-  var dark = overwrite ? overwrite : document.getElementById('theme').checked == true
-  document.body.style.cssText = dark ? '--main-background-color: #232323;--main-font-color: rgba(255, 255, 255, 0.87);--script-bg-color: 232323' : '  --main-background-color: #fff;--main-font-color: rgba(0, 0, 0, 0.87);--script-bg-color: rgb(250, 250, 250)'
-  document.getElementById('themeLabel').innerHTML = dark ? "Dark Theme" : "Bright Theme"
+  var dark = overwrite
+    ? overwrite
+    : document.getElementById('theme').checked == true
+  document.body.style.cssText = dark
+    ? '--main-background-color: #232323;--main-font-color: rgba(255, 255, 255, 0.87);--script-bg-color: #232323'
+    : '  --main-background-color: #fff;--main-font-color: rgba(0, 0, 0, 0.87);--script-bg-color: rgb(250, 250, 250)'
+  document.getElementById('themeLabel').innerHTML = dark
+    ? 'Dark Theme'
+    : 'Bright Theme'
   if (localStorage) {
     localStorage.setItem('dark', dark)
   }
 }
 
+//Demo Data for now
+function loadScripts() {
+  for (i = 0; i < champData.length; i++) {
+    var count = Math.floor(Math.random() * 4)
+    for (c = 0; c < count; c++) {
+      scriptData[champData[i].systemName] =
+        scriptData[champData[i].systemName] || []
+      scriptData[champData[i].systemName].push({
+        name: 'Script' + c,
+        author: 'Author' + c,
+        download: 'http://goo.gl/4qagA1'
+      })
+    }
+  }
+}
+
 if (localStorage && localStorage.dark) {
-  document.getElementById('theme').checked = localStorage.dark == "true"
-  setTheme(localStorage.dark == "true")
+  document.getElementById('theme').checked = localStorage.dark == 'true'
+  setTheme(localStorage.dark == 'true')
 }
 
 function loadJSON(path, success, error) {
   var xhr = new XMLHttpRequest()
-  xhr.onreadystatechange = function () {
+  xhr.onreadystatechange = function() {
     if (xhr.readyState === XMLHttpRequest.DONE) {
       if (xhr.status === 200) {
         if (success) success(JSON.parse(xhr.responseText))
@@ -29,12 +53,15 @@ function loadJSON(path, success, error) {
   xhr.send()
 }
 
-var version
-
-
-loadJSON("https://ddragon.leagueoflegends.com/api/versions.json", function (data) {
+loadJSON(
+  'https://ddragon.leagueoflegends.com/api/versions.json',
+  function(data) {
     version = data[0]
-    loadJSON("https://ddragon.leagueoflegends.com/cdn/" + version + "/data/en_US/champion.json", function (data) {
+    loadJSON(
+      'https://ddragon.leagueoflegends.com/cdn/' +
+        version +
+        '/data/en_US/champion.json',
+      function(data) {
         for (champ in data.data) {
           var champObject = data.data[champ]
           champData.push({
@@ -44,57 +71,64 @@ loadJSON("https://ddragon.leagueoflegends.com/api/versions.json", function (data
             title: capitalizeFirstLetter(champObject.title)
           })
         }
-        if (localStorage && !localStorage.data && !localStorage.version) {
-          renderChamps(champData, true)
-        }
+        loadScripts()
+        renderChamps(champData, true)
         if (localStorage) {
-          localStorage.setItem("data", JSON.stringify(champData))
-          localStorage.setItem("version", version)
+          localStorage.setItem('champData', JSON.stringify(champData))
+          localStorage.setItem('version', version)
         }
       },
-      function (error) {
+      function(error) {
         console.error(error)
-      })
+      }
+    )
   },
-  function (error) {
+  function(error) {
     console.error(error)
-  })
-
+  }
+)
 
 var elem = document.querySelector('.collapsible')
 M.Collapsible.init(elem)
 
-var elem = document.querySelectorAll('.tooltipped');
-M.Tooltip.init(elem);
+var elem = document.querySelectorAll('.tooltipped')
+M.Tooltip.init(elem)
 
 var elem = document.querySelector('.modal')
 M.Modal.init(elem, {
-  onCloseEnd: function () {
+  onCloseEnd: function() {
     document.getElementById('modalChampImg').src = ''
   }
 })
 
-
 function renderChamps(data, firstRun = false) {
-  if (!renderChamps) return;
+  if (!renderChamps) return
   var str = ''
   for (i = 0; i < data.length; i++) {
     str += `<div class="col s6 m6 l3 xl2 hoverable champWrapper" onclick=showChamp("${
       data[i].systemName
     }")>
-            <img class="circle champImg" ${
-              firstRun ? 'data-' : ''
-            }src="https://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${
+            <div class="champImgWrapper">
+              <img class="circle champImg" ${
+                firstRun ? 'data-' : ''
+              }src="https://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${
       data[i].systemName
-    }.png"><span class="badge">3</span>
-            <p class="flow-text">${data[i].systemName}</p>
+    }.png"><span class="badge ${
+      scriptData[data[i].systemName]
+        ? ''
+        : 'grey darken-3 grey-text text-darken-2'
+    }">${
+      scriptData[data[i].systemName] ? scriptData[data[i].systemName].length : 0
+    }</span>
+            </div>
+            <p class="flow-text">${data[i].name}</p>
         </div>`
   }
   document.querySelector('#list').innerHTML = str
-  if (!firstRun) return;
-  [].forEach.call(document.querySelectorAll('img[data-src]'), function (img) {
+  if (!firstRun) return
+  ;[].forEach.call(document.querySelectorAll('img[data-src]'), function(img) {
     img.setAttribute('src', img.getAttribute('data-src'))
-    img.onload = function () {
+    img.onload = function() {
       img.removeAttribute('data-src')
     }
   })
@@ -103,47 +137,33 @@ function renderChamps(data, firstRun = false) {
 document.getElementById('searchField').addEventListener('keyup', filterData)
 
 function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
+  return string.charAt(0).toUpperCase() + string.slice(1)
 }
 
 function filterData() {
   //Search
   var filter = document.getElementById('searchField').value.toLowerCase()
-  var checkbox = document.querySelector('input[name="roleFilter"]:checked').value;
+  var checkbox = document.querySelector('input[name="roleFilter"]:checked')
+    .value
   var newList = []
   for (i = 0; i < champData.length; i++) {
-    if (champData[i].name.toLowerCase().includes(filter) || champData[i].systemName.toLowerCase().includes(filter)) {
-      if (checkbox == "All" || champData[i].tags.includes(checkbox))
+    if (
+      champData[i].name.toLowerCase().includes(filter) ||
+      champData[i].systemName.toLowerCase().includes(filter)
+    ) {
+      if (checkbox == 'All' || champData[i].tags.includes(checkbox))
         newList.push(champData[i])
     }
   }
   renderChamps(newList)
 }
 
-//Demo Data
-var scripts = [{
-    name: 'Some script',
-    description: 'Some text',
-    download: 'https://www.google.com'
-  },
-  {
-    name: 'Some random script',
-    description: 'Some good text',
-    download: 'https://www.google.com'
-  },
-  {
-    name: 'Some bad script',
-    description: 'Some funny text',
-    download: 'https://www.google.com'
-  }
-]
-
 function showChamp(champName) {
   var instance = M.Modal.getInstance(elem)
 
   var champ = champData.filter(champ => champ.systemName == champName)[0]
 
-  if (!champ) return;
+  if (!champ) return
 
   document.getElementById('modalChampName').innerHTML = champ.name
   document.getElementById('modalChampTitle').innerHTML = champ.title
@@ -151,25 +171,32 @@ function showChamp(champName) {
   img.setAttribute(
     'data-src',
     'http://ddragon.leagueoflegends.com/cdn/img/champion/loading/' +
-    champ.name +
-    '_0.jpg'
+      champ.systemName +
+      '_0.jpg'
   )
 
   img.setAttribute('src', img.getAttribute('data-src'))
-  img.onload = function () {
+  img.onload = function() {
     img.removeAttribute('data-src')
   }
 
   //CARE FOR XSS!
-  var str = ''
-  for (i = 0; i < scripts.length; i++) {
+  var script = scriptData[champ.systemName] || []
+  console.log(script)
+  var str =
+    script.length == 0
+      ? '<div class="center noFound"><span class="flow-text nonFound grey-text text-darken-2">No scripts found for ' +
+        champ.name +
+        '</span><div>'
+      : ''
+  for (i = 0; i < script.length; i++) {
     var stars = Math.floor(Math.random() * 5) + 1
     str += `<li class="collection-item avatar">
-        <a href="${scripts[i].download}">
+        <a href="${script[i].download}">
             <i class="material-icons circle">file_download</i>
         </a>
-        <span class="title">${scripts[i].name}</span>
-        <p>${scripts[i].description}</p>
+        <span class="title">${script[i].name}</span>
+        <p>by ${script[i].author}</p>
         <a href="#!" class="secondary-content">
             ${'<i class="material-icons">star</i>'.repeat(stars)}
             ${'<i class="material-icons">star_outline</i>'.repeat(5 - stars)}
@@ -183,7 +210,8 @@ function showChamp(champName) {
   instance.open()
 }
 
-if (localStorage && localStorage.data && localStorage.version) {
+if (localStorage && localStorage.champData && localStorage.version) {
   version = localStorage.version
-  renderChamps(JSON.parse(localStorage.data), true)
+  loadScripts()
+  renderChamps(JSON.parse(localStorage.champData), true)
 }
